71b8d44da9c23819b0d85206d74c51f6484903cd
Anne Watkins Stahlfeld <anne.watkins6@gmail.com>
2023-02-03 14:38:50 -0600

    Add example weather files
    as of Feb 3, weather must still run on comps/calculon

--- Week2/extract_weather.py
+++ Week2/extract_weather.py
@@ -6,12 +6,11 @@ from emodpy_malaria.weather import generate_weather
 # ---| Request weather files |---
 
 # Request weather time series, for nodes listed in a .csv file
-wr = generate_weather(platform="SLURM_LOCAL",
-                      site_file="../inputs/Mutasa.csv",
+wr = generate_weather(platform="Calculon",
+                      site_file="../inputs/example_site.csv",
                       start_date=2019001,
                       end_date=2019365,
                       node_column="nodes",
-                      local_dir="../inputs/Mutasa/",
-                      is_staging=True)
+                      local_dir="../inputs/example_weather/")
 
 print("\n".join(wr.files))
--- Week2/recreate_weather.py
+++ Week2/recreate_weather.py
@@ -1,20 +1,21 @@
 import os
-?
+
 import pandas as pd
 from emodpy_malaria.weather import *
 from emodpy_malaria.weather.weather_variable import WeatherVariable
-?
-indir = "/home/aew2948/FE-2023-examples/Week2"
+
+#indir = "/home/aew2948/FE-2023-examples/Week2"
+indir = "C:/Users/annew/OneDrive/Documents/GitHub/FE-2023-examples/inputs/example_weather"
 df, wa = weather_to_csv(indir)
 df.to_csv(os.path.join(indir, "output.csv")) # Make a copy of CSV for manipulation if needed
-?
+
 df1 = df.copy()
 df1['date'] = pd.date_range('2019-01-01', '2019-12-31')
 df1['nodes'] = 1
 
 # Create EMOD-readable weather files to be used in simulations from modified CSVs if needed
 wa.update({"IdReference": "Custom user"})
-?
+
 weather_columns = {
     WeatherVariable.AIR_TEMPERATURE: "airtemp",
     WeatherVariable.RELATIVE_HUMIDITY: "humidity",
@@ -22,12 +23,12 @@ weather_columns = {
     WeatherVariable.LAND_TEMPERATURE: "landtemp"
 }
 weather_filenames = {
-    WeatherVariable.AIR_TEMPERATURE: "Mutasa_air_temperature_daily.bin",
-    WeatherVariable.RELATIVE_HUMIDITY: "Mutasa_relative_humidity_daily.bin",
-    WeatherVariable.RAINFALL: "Mutasa_rainfall_daily.bin",
-    WeatherVariable.LAND_TEMPERATURE: "Mutasa_land_temperature_daily.bin"
+    WeatherVariable.AIR_TEMPERATURE: "example_air_temperature_daily.bin",
+    WeatherVariable.RELATIVE_HUMIDITY: "example_relative_humidity_daily.bin",
+    WeatherVariable.RAINFALL: "example_rainfall_daily.bin",
+    WeatherVariable.LAND_TEMPERATURE: "example_land_temperature_daily.bin"
 }
-?
+
 csv_to_weather(df1, attributes=wa, weather_columns=weather_columns,
                weather_dir=os.path.join(indir, "out"),
                weather_file_names=weather_filenames)
