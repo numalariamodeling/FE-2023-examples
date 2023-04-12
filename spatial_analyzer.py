048e7fc423050f2bc50259d938224cc9a247719a
tobias-holden <52420991+tobias-holden@users.noreply.github.com>
2023-04-12 09:14:56 -0500

    Update spatial_analyzer.py
    adding descriptive comments

--- spatial_analyzer.py
+++ spatial_analyzer.py
@@ -1,8 +1,9 @@
 import struct
 import numpy as np
 
+# Defines SpatialOutput Class #
+###############################
 class SpatialOutput:
-
     def __init__(self):
         self.n_nodes = 0
         self.n_tstep = 0
@@ -11,7 +12,7 @@ class SpatialOutput:
         self.start = 0
         self.interval = 1
 
-    @classmethod
+    @classmethod # to decode .bin and return .json
     def from_bytes(cls, bytes, filtered=False):
         # The header size changes if the file is a filtered one
         headersize = 16 if filtered else 8
@@ -50,9 +51,12 @@ class SpatialOutput:
                 'interval': self.interval,
                 'data': self.data}
 
+
+
+# Function to convert .json to .csv #
+#####################################
 from typing import Dict
 import pandas as pd
-
 def construct_spatial_output_df(rawdata: Dict, channel: str, timesteps=None) -> pd.DataFrame:
     """
     Construct spatial output data frame from a Spatial Output dictionary
@@ -85,26 +89,29 @@ def construct_spatial_output_df(rawdata: Dict, channel: str, timesteps=None) -> 
     timesteps = sorted(list(set(all_timesteps).intersection(timesteps)))
     return df[df['time'].isin(timesteps)]
 
+# SpatialAnalyzer - collect outputs #
+#####################################
 
 import os
 import pandas as pd
-
 from idmtools.entities import IAnalyzer
 from idmtools.entities.simulation import Simulation
 
 class SpatialAnalyzer(IAnalyzer):
-    # This analyzer can handled both unfiltered and filtered SpatialReports
-    def __init__(self, dir_name, exp_id, spatial_channels, sweep_variables, working_dir='.', snapshot=None):
+    # This analyzer can handle both unfiltered and filtered SpatialReports
+    def __init__(self, dir_name, f_base, f_suffix, exp_id, spatial_channels, sweep_variables, working_dir='.', snapshot=None):
         super(SpatialAnalyzer, self).__init__(working_dir=working_dir,
                                                  filenames=[f'output/SpatialReportMalariaFiltered_all_ages_{x}.bin' for x in spatial_channels])
 
         self.dir_name = dir_name
+        self.f_base = f_base or 'SpatialReportMalariaFiltered'
+        self.f_suffix = f_suffix or ''
         self.exp_id = exp_id
         # Once we fix idmtools, we should remove this
         self.parse = False
         self.sweep_variables = sweep_variables or ['Run_Number', 'x_Temporary_Larval_Habitat']
         self.spatial_channels = spatial_channels
-        self.output_fname = os.path.join(self.working_dir, 'spatial_output',self.dir_name, f"SpatialReportMalariaFiltered.csv")
+        self.output_fname = os.path.join(self.working_dir, 'spatial_output',self.dir_name, f"{self.f_base}_{self.f_suffix}.csv")
         self.snapshot = snapshot
         
 
@@ -113,18 +120,15 @@ class SpatialAnalyzer(IAnalyzer):
 
     def map(self, data, simulation: Simulation):
         # we have to parse our data first since it will be a raw set of binary data
-        # Once we have this fixed within idmtools/emodpy, we will remove this bit of code
         for ch in self.spatial_channels:
-            fn = f'output/SpatialReportMalariaFiltered_all_ages_{ch}.bin'
+            fn = f'output/{self.f_base}{self.f_suffix}_{ch}.bin'
             data[fn] = SpatialOutput.from_bytes(data[fn], 'Filtered' in fn).to_dict()
-        simdata = construct_spatial_output_df(data[f'output/SpatialReportMalariaFiltered_all_ages_{self.spatial_channels[0]}.bin'], self.spatial_channels[0])
+        simdata = construct_spatial_output_df(data[f'output/{self.f_base}_{self.f_suffix}_{self.spatial_channels[0]}.bin'], self.spatial_channels[0])
         if len(self.spatial_channels) > 1:
             for ch in self.spatial_channels[1:]:
                 simdata = pd.merge(left=simdata,
-                                   right=construct_spatial_output_df(data[f'output/SpatialReportMalariaFiltered_all_ages_{ch}.bin'], ch),
+                                   right=construct_spatial_output_df(data[f'output/{self.f_base}_{self.f_suffix}_{ch}.bin'], ch),
                                    on=['time', 'node'])
-
-        # simdata['time'] = simdata['time'] - 365
         for sweep_var in self.sweep_variables:
             if sweep_var in simulation.tags.keys():
                 simdata[sweep_var] = simulation.tags[sweep_var]
@@ -153,16 +157,14 @@ class SpatialAnalyzer(IAnalyzer):
             # save snapshots
             if self.snapshot is not None:
                 d_sub = d[d['time'] == self.snapshot[0]] 
-                sub_fname = os.path.join(self.working_dir, 'spatial_output',self.dir_name,f"SpatialReportMalariaFiltered_Snapshot.csv")
+                sub_fname = os.path.join(self.working_dir, 'spatial_output',self.dir_name,f"{self.f_base}_{self.f_suffix}_Snapshot.csv")
                 if(len(self.snapshot)>1):
                     for snap in self.snapshot[1:]:
                         d_sub_add = d[d['time'] == snap]
                         d_sub = pd.concat([d_sub,d_sub_add])
                 d_sub.to_csv(sub_fname)
                 print("Snapshot from days",self.snapshot, "saved to",sub_fname) 
-            
-
-
+                
         
 if __name__ == "__main__":
 
@@ -170,37 +172,44 @@ if __name__ == "__main__":
     from idmtools.core import ItemType
     from idmtools.core.platform_factory import Platform
     
+    ## Experiments Dictionary ##
+    ############################
     # {'experiment label' : 'exp_id'}
     expts = {'FE_example' : '9729c597-1161-4631-a222-ac1be450887c'}
+    ## Paths ##
+    ###########
     # experiments folder
     jdir =  '/projects/b1139/indie_emodpy/experiments'
     # output folder
     wdir=os.path.join('/projects/b1139/indie_emodpy/simulation_output', 'baseline')
-    
     if not os.path.exists(wdir):
         os.mkdir(wdir)
-    # Define sweep variables
-    sweep_variables = ['Run_Number', 'xTLH'] 
-    # Spatial channels from list provided to add_spatial_report_malaria_filtered() 
-    spatial_channels = ['Population', 'PCR_Parasite_Prevalence','New_Clinical_Cases']
+    ## Analyzer Specifications ##
+    #############################
+    # Grouping variables (for each node & timestep)
+    sweep_variables = ['Run_Number', 'xTLH']   
+    # Outputs to analyze - must have been requested during simulation
+    spatial_channels = ['Population',           
+                        'PCR_Parasite_Prevalence',
+                        'New_Clinical_Cases']
+    # SpatialReportMalariaFiltered or SpatialReport?
+    report_type = 'SpatialReportMalariaFiltered'    
+    # .bin File suffix? (use empty string '' if none) 
+    report_suffix = 'all_ages'
     
+    ## Run Analyzer ##
+    ##################
     with Platform('SLURM_LOCAL',job_directory=jdir) as platform:
         for expname, exp_id in expts.items():
             analyzer = [SpatialAnalyzer(dir_name=expname,
+                                        f_base = report_type,
+                                        f_suffix = report_suffix,
                                         exp_id = exp_id,
                                         spatial_channels=spatial_channels,
                                         sweep_variables=sweep_variables,
-                                        working_dir=wdir)]
-            
+                                        working_dir=wdir)]      
             # Create AnalyzerManager with required parameters
             manager = AnalyzeManager(configuration={},ids=[(exp_id, ItemType.EXPERIMENT)],
                                      analyzers=analyzer, partial_analyze_ok=True)
             # Run analyze
             manager.analyze()
-    
-    # PLOTTING? 
-    make_plots = False
-    
-    if make_plots:
-        from plotnine.data import mpg
-        from plotnine import ggplot, aes, labs, geom_path
