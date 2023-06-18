605023bd1c22ce26793d17e143297447b67087f9
tobias-holden <52420991+tobias-holden@users.noreply.github.com>
2023-06-18 18:04:41 -0500

    Update quest_test.sh

--- prep/quest_intro/quest_test.sh
+++ prep/quest_intro/quest_test.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #SBATCH --account=b1139  
 #SBATCH --partition=b1139
-#SBATCH --nodes=1 ## how many computers do you need
-#SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
-#SBATCH --time=00:10:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=1G ## how much RAM do you need per CPU, also see --mem=<XX>G for RAM per node/computer (this effects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name=sample_job  ## When you run squeue -u NETID this is how you can identify the job
-#SBATCH --output=outlog ## standard out and standard error goes to this file
-#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
-#SBATCH --mail-user=<email>@northwestern.edu ## your email
+#SBATCH --nodes=1                            ## how many computers do you need?
+#SBATCH --ntasks-per-node=1                  ## how many cpus or processors do you need on each computer?
+#SBATCH --time=00:10:00                      ## time needed to run (HH:MM:SS)
+#SBATCH --mem-per-cpu=1G                     ## RAM per CPU, also see --mem=<XX>G for RAM per node/computer 
+#SBATCH --job-name=quest_example             ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=outlog                      ## standard output goes to this file
+#SBATCH --error=errlog                       ## standard error goes to this file
+#SBATCH --mail-type=ALL                      ## receive e-mail alerts from SLURM when your job begins and finishes
+#SBATCH --mail-user=<email>@northwestern.edu 
 
 module purge all
 module load python
 
 python --version
-python quest_test.py
+python quest_test.py --name STRANGER  # Replace with your name to supply to python script
