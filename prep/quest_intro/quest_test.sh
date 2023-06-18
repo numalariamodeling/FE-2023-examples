5bdda83245a042b6d72348ccb4a6e695c6632de0
tobias-holden <52420991+tobias-holden@users.noreply.github.com>
2023-06-18 11:25:10 -0500

    Create quest_test.sh

---
+++ prep/quest_intro/quest_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=b1139  
+#SBATCH --partition=b1139
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
+#SBATCH --time=00:10:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=1G ## how much RAM do you need per CPU, also see --mem=<XX>G for RAM per node/computer (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=sample_job  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=outlog ## standard out and standard error goes to this file
+#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
+#SBATCH --mail-user=<email>@northwestern.edu ## your email
+
+module purge all
+module load python
+
+python --version
+python quest_test.py
