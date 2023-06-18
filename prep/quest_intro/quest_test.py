c96f87e038160d841adf814725f31c2eb5433e44
tobias-holden <52420991+tobias-holden@users.noreply.github.com>
2023-06-18 18:01:50 -0500

    Update quest_test.py

--- prep/quest_intro/quest_test.py
+++ prep/quest_intro/quest_test.py
@@ -1,14 +1,13 @@
 import argparse
 import time
+from datetime import date
 
 
 def parse_commandline():
     """Parse the arguments given on the command-line.
     """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--job-id",
-                       help="Job number",
-                       default=0)
+    parser.add_argument("--name", type=str, required=True)
 
     args = parser.parse_args()
 
@@ -21,4 +20,4 @@ def parse_commandline():
 if __name__ == '__main__':
     args = parse_commandline()
     time.sleep(10) # Sleep for 3 seconds
-    print(args.job_id)
+    print("Hello " + args.name + ", today is " + str(date.today()))
